6834d89836d94ec5943abcece0ae680ad6f3e138
Sameera Abeysekara <sameeraa@boardpac.org>
2021-09-11 11:57:54 +0530

    1st commit

---
+++ rollBack/DataModel/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rollBack.DataModel
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        //public string Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
---
+++ rollBack/Migrations/20210911061709_removelastName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rollBack.Migrations
+{
+    public partial class removelastName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lastname",
+                table: "Employees");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Lastname",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
